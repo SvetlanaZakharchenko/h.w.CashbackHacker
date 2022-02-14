77b85a2905a49394841de8e119e83cd5d457f8ed
SvetlanaZakharchenko <sveta-class@mail.ru>
2022-02-14 13:41:30 +0300

    added test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,52 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+
+    CashbackHackService cashbackHackService = new CashbackHackService();
+    @Test
+    public void showBelowBoundary() {
+        int amount = 100;
+        int expected = 900;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void showHigherBoundary () {
+        int amount = 1900;
+        int expected = 100;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void showNullBoundary () {
+        int amount = 0;
+        int expected = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void showEqualBoundary () {
+        int amount = 1000;
+        int expected = 1000;
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(actual, expected);
+    }
+
 
 }
