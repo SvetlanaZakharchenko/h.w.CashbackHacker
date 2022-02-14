01f601a492acab65f557477394753cc4e904b55d
SvetlanaZakharchenko <sveta-class@mail.ru>
2022-02-14 14:25:55 +0300

    changed build.gradle and test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
